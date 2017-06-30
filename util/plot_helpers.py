9a1fc69c94860165b75be39936c3f7bac7022dd5
Udo.Dehm <udo.dehm@mailbox.org>
2017-06-30 17:44:43 +0200

    added plotting function for tensorflow graph in jupyter notebook

--- util/plot_helpers.py
+++ util/plot_helpers.py
@@ -3,8 +3,11 @@
 """                                              
 Usefull functions for plotting
 """                                              
-                                                 
+
+import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
+from IPython.display import clear_output, Image, display, HTML
 
 __author__ = "Udo Dehm"                          
 __copyright__ = "Copyright 2017"                 
@@ -36,3 +39,49 @@ def plot_images(images, titles='', *args, **kwargs):
         plt.figure(*args, **kwargs)
         plt.title(title)
         plt.imshow(image)
+
+
+# the following two functions are for plotting a tensorflow graph in a jupyter
+# notebook file, see
+# https://stackoverflow.com/questions/38189119/simple-way-to-visualize-a-tensorflow-graph-in-jupyter
+def _strip_consts(graph_def, max_const_size=32):
+    """
+    Strip large constant values from graph_def.
+    """
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = "<stripped %d bytes>"%size
+    return strip_def
+
+
+def show_graph(graph_def, max_const_size=32): 
+    """
+    Visualize TensorFlow graph.
+    """
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    
+    strip_def = _strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+            function load() {{
+                document.getElementById("{id}").pbtxt = {data};
+            }}
+        </script> 
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+            <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+    
+    iframe = """ 
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+
