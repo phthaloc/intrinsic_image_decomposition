c304cf950848dd54d7603d8c2c9ecb7afe9f735e
Udo.Dehm <udo.dehm@mailbox.org>
2017-09-07 17:41:11 +0200

    added function for calculating channel-based mean of list of images

--- util/general_helpers.py
+++ util/general_helpers.py
@@ -6,6 +6,9 @@ General smaller, usefull helper functions.
 
 import os
 import logging
+import numpy as np
+import scipy as sp
+
 
 __author__ = "Udo Dehm"                          
 __copyright__ = "Copyright 2017"                 
@@ -17,7 +20,28 @@ __email__ = "udo.dehm@mailbox.org"
 __status__ = "Development"                       
                                                  
                                                  
-__all__ = ['get_time_format', 'time_tuple_to_str', 'create_logger']   
+__all__ = ['channel_means', 'get_time_format', 'time_tuple_to_str', 'create_logger']   
+
+
+def channel_means(image_list, num_channels=3):
+    """
+    Calculates the channel-based mean of a list of images given as a list of
+    image paths
+    :param image_list: list of image paths
+    :type image_list: list, pd.Series, etc.
+    :param num_channels: number of chennels of the images (default: 3)
+    :type num_channels: int
+    :return: mean for each channel accros all images
+    """
+    sum_px = np.zeros(num_channels)
+    count_px = np.zeros(num_channels)
+    for path in image_list:
+        img = sp.misc.imread(name=path, flatten=False, mode='RGB') 
+        channels = np.split(ary=img, indices_or_sections=num_channels, axis=2)
+        for i in range(num_channels):
+            sum_px[i] += channels[i].sum()
+            count_px[i] += np.prod(channels[i].shape)
+    return sum_px / count_px
 
 
 def get_time_format(time_in_sec):
@@ -32,6 +56,7 @@ def get_time_format(time_in_sec):
     secs = int(time_in_sec % 3600 % 60)
     return hours, mins, secs
 
+
 def time_tuple_to_str(time_tuple):
     s = '{h:02}:{m:02}:{s:02}'.format(h=time_tuple[0], m=time_tuple[1],
                                       s=time_tuple[2])
@@ -44,6 +69,7 @@ def time_tuple_to_str(time_tuple):
             s = s[6:] + ' sec'
             s = s.lstrip('0')
     return s
+
 
 def create_logger(filename):
     """
@@ -73,3 +99,4 @@ def create_logger(filename):
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     return logger
+
