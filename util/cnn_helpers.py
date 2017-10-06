c0d6bf4e309c6e9ebf546a2c5273b67e634d4257
Udo.Dehm <udo.dehm@mailbox.org>
2017-10-06 18:57:06 +0200

    refactoring so that function compute_whdr_tf() now takes only tf objects as input params

--- util/cnn_helpers.py
+++ util/cnn_helpers.py
@@ -419,21 +419,34 @@ def loss_fct(label_albedo, label_shading, prediction_albedo,
     return loss
 
 
-def compute_whdr_tf(reflectance, df_judgements, delta=0.1):
+def compute_whdr_tf(reflectance, point1, point2, human_labels, weights,
+                    delta=0.1):
     """
     This is the Tensorflow implementation of the
     util.whdr_py3.compute_whdr() function
     Return the WHDR score for a reflectance image, evaluated against human
     judgements.  The return value is in the range 0.0 to 1.0.
     See section 3.5 of our paper for more details.
-    :param reflectance: a tf tensor containing the linear RGB reflectance
-        image.
+    :param reflectance: a tf tensor containing linear RGB reflectance
+        images (in batches).
     :type reflectance: tf. tensor of shape
         ['batch', 'height', 'width', channels]
-    :param df_judgements: a pd.DataFrame object loaded and preprocessed from
-        the Intrinsic Images in the Wild dataset.
-    :type df_judgements: pd.DataFrame with columns
-        ['batch_nr', 'darker', 'darker_score', 'x1', 'x2', 'y1', 'y2']
+    :param point1: list of point1 comparisons indices (these points are compared
+        to corresponding entries of point2)
+    :type point1: tf tensor object with shape ['batch', 'height', 'width'] and
+        dtype=tf.int32
+    :param point2: list of point2 comparisons indices (these points are compared
+        to corresponding entries of point1)
+    :type point2: tf tensor object with shape ['batch', 'height', 'width'] and
+        dtype=tf.int32
+    :param human_labels: list of human labels (according to iiw data set)
+        corresponding to point1 and point2 entries
+    :type human_labels: tf tensor object with shape ['darker'] and
+        dtype=tf.int32
+    :param weights: list of weights/ darker scores/ darker weights, 
+        corresponding to point1, point2 and human_labels entries
+    :type weights: tf tensor object with shape ['darker_score'] and 
+        dtype=tf.float32
     :param delta: the threshold where humans switch from saying "about the same"
         to "one point is darker."
     :type delta: float
@@ -445,12 +458,9 @@ def compute_whdr_tf(reflectance, df_judgements, delta=0.1):
     # that represent point2 over all samples in the batch. Points in
     # comp_p1_batch and comp_p2_batch are in the same order (important for
     # comparison)):
-    comp_p1_batch = tf.gather_nd(imgs_grey_tf,
-                                 [list(x) for x in df_judgements[['batch_nr',
-                                                            'y1', 'x1']].values])
-    comp_p2_batch = tf.gather_nd(imgs_grey_tf,
-                                 [list(x) for x in df_judgements[['batch_nr',
-                                                            'y2', 'x2']].values])
+    comp_p1_batch = tf.gather_nd(imgs_grey_tf, point1)
+    comp_p2_batch = tf.gather_nd(imgs_grey_tf, point2)
+    
     # define/convert to pixel threshold:
     comp_p1_batch = tf.maximum(comp_p1_batch, 1e-10)
     comp_p2_batch = tf.maximum(comp_p2_batch, 1e-10)
@@ -475,18 +485,13 @@ def compute_whdr_tf(reflectance, df_judgements, delta=0.1):
     # combine above by summing (-> 0 belongs to approx the same darkness):
     darker = px1_darker + px2_darker
 
-    # get the human darker labels:
-    human_labels = tf.constant(df_judgements['darker'].values, dtype=tf.int32)
     # compare human labels to algorithm darker predictions:
     hum_alg_comp = tf.not_equal(darker, human_labels)
-    # list of weights/darker scores:
-    darker_weights = tf.constant(df_judgements['darker_score'].values,
-                                 dtype=tf.float32)
     # summed weights:
-    weight_sum = tf.reduce_sum(darker_weights)
+    weight_sum = tf.reduce_sum(weights)
     # only select such weights that belong to prediction - human label mismatch:
     error_weights = tf.where(condition=hum_alg_comp,
-                             x=darker_weights,
+                             x=weights,
                              y=tf.zeros_like(hum_alg_comp, dtype=tf.float32))
     # sum all error_weights
     error_sum = tf.reduce_sum(error_weights)
